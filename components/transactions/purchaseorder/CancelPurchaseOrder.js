75117c985de83add0b64d8753c642b1fa4ebd214
domsteil <domsteil14@gmail.com>
2022-08-18 22:24:48 -0700

    updating purchase order page

--- components/transactions/purchaseorder/CancelPurchaseOrder.js
+++ components/transactions/purchaseorder/CancelPurchaseOrder.js
@@ -173,7 +173,7 @@ export default (props) => {
                     </div>
                 </div>
             </Transition.Root>
-            <button onClick={handleOnSubmit} type="button" class="px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+            <button onClick={handleOnSubmit} type="button" class="inline-flex items-center rounded-md border border-gray-300 bg-white px-3 py-2 text-sm font-medium leading-4 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-30">
                Cancel
             </button>
         </main >
