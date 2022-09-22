1d4f211c4384631c6879ef835bd0ae8aeffeba1c
domsteil <domsteil14@gmail.com>
2022-09-22 12:55:13 -0700

    updating

--- pages/api/payments/create-custom-account.js
+++ pages/api/payments/create-custom-account.js
@@ -4,25 +4,25 @@ export default async (req, res) => {
 
     try {
 
-const account = await stripe.accounts.create({
-    country: 'US',
-    type: 'custom',
-    capabilities: {
-        treasury: { requested: true },
-        issuing: { requested: true },
-        card_issuing: { requested: true },
-        card_payments: { requested: true },
-        transfers: { requested: true }
-      },
-  });
+        const account = await stripe.accounts.create({
+            country: 'US',
+            type: 'custom',
+            capabilities: {
+                treasury: { requested: true },
+                issuing: { requested: true },
+                card_issuing: { requested: true },
+                card_payments: { requested: true },
+                transfers: { requested: true }
+            },
+        });
 
 
-  return res.status(200).json({ account_status: 'custom_stateset_account_created' });
+        return res.status(200).json({ account_status: 'custom_stateset_account_created' });
 
-} catch (error) {
-    return res.status(503).send({
-      success: false,
-      error: error.stack
-    });
-  }
+    } catch (error) {
+        return res.status(503).send({
+            success: false,
+            error: error.stack
+        });
+    }
 };
